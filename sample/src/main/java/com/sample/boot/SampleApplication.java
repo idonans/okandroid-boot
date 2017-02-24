4734a32e20243b2923316d3aabc1b47d095250e0
idonans <idonans@126.com>
2017-02-24 18:50:59 +0800

    add LeakCanary for boot sample

--- sample/src/main/java/com/sample/boot/SampleApplication.java
+++ sample/src/main/java/com/sample/boot/SampleApplication.java
@@ -2,6 +2,8 @@ package com.sample.boot;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by idonans on 2017/2/1.
  */
@@ -11,6 +13,13 @@ public class SampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+
         AppInit.init(this);
     }
 
