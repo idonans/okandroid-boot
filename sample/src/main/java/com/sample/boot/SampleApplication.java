582a1ae0a611d6097dfc33718d620ce7cf01041d
idonans <idonans@126.com>
2017-03-02 18:09:00 +0800

    improve SampleApplication init for LeakCanary

--- sample/src/main/java/com/sample/boot/SampleApplication.java
+++ sample/src/main/java/com/sample/boot/SampleApplication.java
@@ -13,12 +13,9 @@ public class SampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
+        if (!LeakCanary.isInAnalyzerProcess(this)) {
+            LeakCanary.install(this);
         }
-        LeakCanary.install(this);
 
         AppInit.init(this);
     }
