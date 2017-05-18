ad9e40c87baa0bdeeb9dd82039cea27bcecdc7c8
idonans <idonans@126.com>
2017-05-18 12:42:46 +0800

    improve OKAndroidFragment lifecycle log

--- boot/src/main/java/com/okandroid/boot/app/OKAndroidFragment.java
+++ boot/src/main/java/com/okandroid/boot/app/OKAndroidFragment.java
@@ -1,10 +1,10 @@
 package com.okandroid.boot.app;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
-import com.okandroid.boot.App;
 import com.okandroid.boot.lang.Available;
 import com.okandroid.boot.lang.Log;
 
@@ -17,36 +17,68 @@ public class OKAndroidFragment extends Fragment implements Available {
     private boolean mAvailable;
     private boolean mResumed;
 
-    private final String DEBUG_TAG = getClass().getName();
-
-    private boolean isDebug() {
-        return App.getBuildConfigAdapter().isDebug();
-    }
+    private final String CLASS_NAME = getClass().getName();
 
     @Override
     public void onStart() {
-        if (isDebug()) {
-            Log.d(DEBUG_TAG + " onStart");
-        }
+        Log.v(CLASS_NAME, "onStart");
 
         super.onStart();
     }
 
     @Override
     public void onPause() {
-        if (isDebug()) {
-            Log.d(DEBUG_TAG + " onPause");
-        }
+        Log.v(CLASS_NAME, "onPause");
 
         super.onPause();
         mResumed = false;
     }
 
     @Override
+    public void onSaveInstanceState(Bundle outState) {
+        Log.v(CLASS_NAME, "onSaveInstanceState");
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        Log.v(CLASS_NAME, "onViewStateRestored");
+
+        super.onViewStateRestored(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.v(CLASS_NAME, "onActivityCreated");
+
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        Log.v(CLASS_NAME, "onAttach");
+
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        Log.v(CLASS_NAME, "onDetach");
+
+        super.onDetach();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.v(CLASS_NAME, "onDestroyView");
+
+        super.onDestroyView();
+    }
+
+    @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
-        if (isDebug()) {
-            Log.d(DEBUG_TAG + " onCreate");
-        }
+        Log.v(CLASS_NAME, "onCreate");
 
         super.onCreate(savedInstanceState);
         mAvailable = true;
@@ -54,9 +86,7 @@ public class OKAndroidFragment extends Fragment implements Available {
 
     @Override
     public void onDestroy() {
-        if (isDebug()) {
-            Log.d(DEBUG_TAG + " onDestroy");
-        }
+        Log.v(CLASS_NAME, "onDestroy");
 
         super.onDestroy();
         mAvailable = false;
@@ -69,28 +99,28 @@ public class OKAndroidFragment extends Fragment implements Available {
 
     @Override
     public void onHiddenChanged(boolean hidden) {
+        Log.v(CLASS_NAME, "onHiddenChanged", hidden);
+
         super.onHiddenChanged(hidden);
     }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
+        Log.v(CLASS_NAME, "setUserVisibleHint", isVisibleToUser);
+
         super.setUserVisibleHint(isVisibleToUser);
     }
 
     @Override
     public void onStop() {
-        if (isDebug()) {
-            Log.d(DEBUG_TAG + " onStop");
-        }
+        Log.v(CLASS_NAME, "onStop");
 
         super.onStop();
     }
 
     @Override
     public void onResume() {
-        if (isDebug()) {
-            Log.d(DEBUG_TAG + " onResume");
-        }
+        Log.v(CLASS_NAME, "onResume");
 
         super.onResume();
         mResumed = true;
