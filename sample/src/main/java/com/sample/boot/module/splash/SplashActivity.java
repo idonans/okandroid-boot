1f203b15b382dad216ff3bbd8d8898371d8540de
idonans <idonans@126.com>
2017-06-20 19:29:47 +0800

    fix sample code of SplashActivity content full view

--- sample/src/main/java/com/sample/boot/module/splash/SplashActivity.java
+++ sample/src/main/java/com/sample/boot/module/splash/SplashActivity.java
@@ -1,9 +1,11 @@
 package com.sample.boot.module.splash;
 
+import android.view.View;
 import android.view.Window;
 
 import com.okandroid.boot.app.ext.preload.PreloadFragment;
 import com.okandroid.boot.util.SystemUtil;
+import com.okandroid.boot.widget.ContentFullView;
 import com.sample.boot.app.BaseActivity;
 
 public class SplashActivity extends BaseActivity {
@@ -14,6 +16,11 @@ public class SplashActivity extends BaseActivity {
         SystemUtil.setStatusBarTransparent(window);
         SystemUtil.setNavigationBarTransparent(window);
         SystemUtil.setSystemUi(window.getDecorView(), true, true, true, false, false);
+    }
+
+    @Override
+    protected View createDefaultContentView() {
+        return new ContentFullView(this);
     }
 
     @Override
