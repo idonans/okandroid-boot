fd8539b59b874018f20b864629963fe0c58750fa
idonans <idonans@126.com>
2017-06-27 12:14:05 +0800

    remove old SystemUtil#setSystemUi, add values-v themes support translucent status bar, nav bar

--- boot/src/main/java/com/okandroid/boot/util/SystemUtil.java
+++ boot/src/main/java/com/okandroid/boot/util/SystemUtil.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.Nullable;
@@ -12,7 +11,6 @@ import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -165,75 +163,11 @@ public class SystemUtil {
                 view.getPaddingLeft(), view.getPaddingTop(), view.getPaddingRight(), view.getPaddingBottom());
     }
 
-    public static void setSystemUi(View view,
-                                   boolean hideStatusBar, boolean lightStatusBar) {
-        setSystemUi(view, true, hideStatusBar, false, false, lightStatusBar);
-    }
-
-    public static void setSystemUi(View view,
-                                   boolean layoutStatusBar, boolean hideStatusBar,
-                                   boolean layoutNavigationBar, boolean hideNavigationBar,
-                                   boolean lightStatusBar) {
-        int flag = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-        if (layoutStatusBar) {
-            flag |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
-        }
-        if (layoutNavigationBar) {
-            flag |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-        }
-
-        if (hideStatusBar) {
-            flag |= View.SYSTEM_UI_FLAG_FULLSCREEN;
-        }
-        if (hideNavigationBar) {
-            flag |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        }
-        if (lightStatusBar) {
-            if (Build.VERSION.SDK_INT >= 23) {
-                flag |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
-            }
-        }
-
-        view.getRootView().setSystemUiVisibility(flag);
-    }
-
-    public static void setStatusBarTransparent(Window window) {
-        setStatusBarTransparent(window, false);
-    }
-
-    public static void setStatusBarTransparent(Window window, boolean clearTranslucentFlag) {
-        if (Build.VERSION.SDK_INT >= 19) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }
-        if (Build.VERSION.SDK_INT >= 21) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
-        if (Build.VERSION.SDK_INT >= 23) {
-            // 部分手机上发现 bug, 此处设置完全透明状态栏的 API 调整到最低 6.0
-            if (clearTranslucentFlag) {
-                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            }
-        }
-    }
-
-    public static void setNavigationBarTransparent(Window window) {
-        setNavigationBarTransparent(window, false);
-    }
-
-    public static void setNavigationBarTransparent(Window window, boolean clearTranslucentFlag) {
-        if (Build.VERSION.SDK_INT >= 19) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        }
-        if (Build.VERSION.SDK_INT >= 21) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setNavigationBarColor(Color.TRANSPARENT);
-        }
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (clearTranslucentFlag) {
-                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            }
-        }
+    public static void setLayoutFullscreen(View view) {
+        view.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
     }
 
     /**
