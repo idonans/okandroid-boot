27aaf4aa06362bdbbf02553d781457d652857750
idonans <idonans@126.com>
2017-07-14 18:38:05 +0800

    use popup window instead system dialog

--- boot/src/main/java/com/okandroid/boot/widget/ContentDialog.java
+++ boot/src/main/java/com/okandroid/boot/widget/ContentDialog.java
@@ -1,39 +1,143 @@
 package com.okandroid.boot.widget;
 
-import android.app.Dialog;
-import android.content.Context;
-import android.os.Bundle;
+import android.app.Activity;
+import android.support.annotation.CallSuper;
+import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
-import android.support.annotation.StyleRes;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.PopupWindow;
 
 import com.okandroid.boot.R;
-import com.okandroid.boot.util.SystemUtil;
+import com.okandroid.boot.lang.ClassName;
+import com.okandroid.boot.util.ViewUtil;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by idonans on 2017/5/15.
  */
 
-public class ContentDialog extends Dialog {
+public abstract class ContentDialog extends PopupWindow {
 
-    public ContentDialog(@NonNull Context context) {
-        this(context, R.style.OKAndroid_Theme_ContentDialog);
+    private final String CLASS_NAME = ClassName.valueOf(this);
+
+    protected Activity mActivity;
+    protected ViewGroup mContentParent;
+    protected LayoutInflater mInflater;
+
+    public ContentDialog(@NonNull Activity activity) {
+        ViewGroup contentParent = ViewUtil.findViewByID(activity, R.id.okandroid_content);
+        if (contentParent == null) {
+            contentParent = ViewUtil.findViewByID(activity, Window.ID_ANDROID_CONTENT);
+        }
+
+        init(activity, contentParent);
     }
 
-    public ContentDialog(@NonNull Context context, @StyleRes int themeResId) {
-        super(context, themeResId);
+    public ContentDialog(@NonNull Activity activity, @NonNull ViewGroup contentParent) {
+        init(activity, contentParent);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        updateSystemUi();
+    private void init(@NonNull Activity activity, @NonNull ViewGroup contentParent) {
+        mActivity = activity;
+        mContentParent = contentParent;
+        mInflater = mActivity.getLayoutInflater();
+        setClippingEnabled(false);
+        setFocusable(true);
+        setTouchable(true);
+        setOutsideTouchable(false);
 
-        super.onCreate(savedInstanceState);
+        setAnimationStyle(0);
+
+        setOnDismissListener(new OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                Log.v(CLASS_NAME, "onDismiss");
+            }
+        });
+
+        onCreate();
     }
 
-    protected void updateSystemUi() {
-        Window window = getWindow();
-        SystemUtil.setLayoutFullscreen(window.getDecorView());
+    protected boolean mDimBackground;
+    protected float mDimAmount;
+
+    public void setDimAmount(float dimAmount) {
+        mDimAmount = dimAmount;
+    }
+
+    public void setDimBackground(boolean dimBackground) {
+        mDimBackground = dimBackground;
+    }
+
+    @CallSuper
+    public void onCreate() {
+    }
+
+    protected int mTargetGravity = Gravity.FILL;
+    protected int mTargetOffsetX = 0;
+    protected int mTargetOffsetY = 0;
+    protected int mTargetWidth = ViewGroup.LayoutParams.MATCH_PARENT;
+    protected int mTargetHeight = ViewGroup.LayoutParams.MATCH_PARENT;
+
+    public void setContentView(int layoutResId) {
+        FrameLayout tmpParent = new FrameLayout(mActivity);
+        View contentView = mInflater.inflate(layoutResId, tmpParent, false);
+        FrameLayout.LayoutParams contentParams = (FrameLayout.LayoutParams) contentView.getLayoutParams();
+        mTargetGravity = contentParams.gravity;
+        mTargetOffsetX = contentParams.leftMargin;
+        mTargetOffsetY = contentParams.topMargin;
+        mTargetWidth = contentParams.width;
+        mTargetHeight = contentParams.height;
+
+        setContentView(contentView);
+        setWidth(mTargetWidth);
+        setHeight(mTargetHeight);
+    }
+
+    public void show() {
+        showAtLocation(mContentParent, mTargetGravity, mTargetOffsetX, mTargetOffsetY);
+        applyDim();
+    }
+
+    @CheckResult
+    public View getDecorView() {
+        try {
+            Field f = PopupWindow.class.getDeclaredField("mDecorView");
+            f.setAccessible(true);
+            return (View) f.get(this);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void applyDim() {
+        if (!mDimBackground) {
+            return;
+        }
+
+        View decorView = getDecorView();
+        if (decorView == null) {
+            Log.e(CLASS_NAME, "decor view not found");
+            return;
+        }
+
+        try {
+            WindowManager.LayoutParams attrs = (WindowManager.LayoutParams) decorView.getLayoutParams();
+            attrs.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+            attrs.dimAmount = mDimAmount;
+            mActivity.getWindowManager().updateViewLayout(decorView, attrs);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- boot/src/main/java/com/okandroid/boot/widget/ContentLoadingDialog.java
+++ boot/src/main/java/com/okandroid/boot/widget/ContentLoadingDialog.java
@@ -1,11 +1,8 @@
 package com.okandroid.boot.widget;
 
-import android.content.Context;
-import android.os.Bundle;
+import android.app.Activity;
 import android.support.annotation.NonNull;
-import android.support.annotation.StyleRes;
-import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import com.okandroid.boot.R;
@@ -17,66 +14,28 @@ import com.okandroid.boot.util.ViewUtil;
 
 public class ContentLoadingDialog extends ContentDialog {
 
-    public ContentLoadingDialog(@NonNull Context context) {
-        super(context);
-    }
-
-    public ContentLoadingDialog(@NonNull Context context, @StyleRes int themeResId) {
-        super(context, themeResId);
-    }
-
-    private long mAnimationDelay = 500L;
-    private long mAnimationDuration = 500L;
     private ProgressBar mProgressBar;
 
+    public ContentLoadingDialog(@NonNull Activity activity) {
+        super(activity);
+    }
+
+    public ContentLoadingDialog(@NonNull Activity activity, @NonNull ViewGroup contentParent) {
+        super(activity, contentParent);
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate() {
+        super.onCreate();
 
         setContentView(R.layout.okandroid_content_loading_dialog);
 
-        View rootView = getWindow().getDecorView();
-        mProgressBar = ViewUtil.findViewByID(rootView, R.id.progress_bar);
+        mProgressBar = ViewUtil.findViewByID(getContentView(), R.id.progress_bar);
 
-        stopLoadingAnimation();
-    }
+        setDimBackground(true);
+        setDimAmount(0.5f);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        startLoadingAnimation();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        stopLoadingAnimation();
-    }
-
-    private void startLoadingAnimation() {
-        mProgressBar.animate()
-                .alpha(1f)
-                .scaleX(1.1f)
-                .scaleY(1.1f)
-                .setDuration(mAnimationDuration)
-                .setStartDelay(mAnimationDelay)
-                .setInterpolator(new AccelerateDecelerateInterpolator())
-                .start();
-    }
-
-    private void stopLoadingAnimation() {
-        mProgressBar.animate().cancel();
-        mProgressBar.setAlpha(0f);
-        mProgressBar.setScaleX(1f);
-        mProgressBar.setScaleY(1f);
-    }
-
-    public void setAnimationDuration(long animationDuration) {
-        mAnimationDuration = animationDuration;
-    }
-
-    public void setAnimationDelay(long animationDelay) {
-        mAnimationDelay = animationDelay;
+        setAnimationStyle(R.style.OKAndroid_Animation_ContentLoadingTop);
     }
 
 }
