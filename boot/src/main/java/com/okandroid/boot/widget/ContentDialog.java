19c2389509fa9d34c3b3c0debea2459fa2866e60
idonans <idonans@126.com>
2017-07-15 12:24:46 +0800

    improve ContentDialog, add ContentDialog decorView as same as Activity content view padding

--- boot/src/main/java/com/okandroid/boot/widget/ContentDialog.java
+++ boot/src/main/java/com/okandroid/boot/widget/ContentDialog.java
@@ -11,11 +11,15 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.PopupWindow;
 
+import com.okandroid.boot.R;
 import com.okandroid.boot.lang.ClassName;
+import com.okandroid.boot.lang.WeakAvailable;
+import com.okandroid.boot.util.ViewUtil;
 
 import java.lang.reflect.Field;
 
@@ -28,10 +32,17 @@ public abstract class ContentDialog extends PopupWindow {
     private final String CLASS_NAME = ClassName.valueOf(this);
 
     protected Activity mActivity;
+    protected View mActivityContentView;
     protected LayoutInflater mInflater;
 
     public ContentDialog(@NonNull Activity activity) {
         mActivity = activity;
+
+        mActivityContentView = ViewUtil.findViewByID(mActivity, R.id.okandroid_content);
+        if (mActivityContentView == null) {
+            mActivityContentView = ViewUtil.findViewByID(mActivity, Window.ID_ANDROID_CONTENT);
+        }
+
         mInflater = mActivity.getLayoutInflater();
         setClippingEnabled(false);
         setFocusable(true);
@@ -47,6 +58,8 @@ public abstract class ContentDialog extends PopupWindow {
                 Log.v(CLASS_NAME, "onDismiss");
             }
         });
+
+        addWeakLayoutForActivityContentView();
     }
 
     protected boolean mDimBackground;
@@ -62,6 +75,13 @@ public abstract class ContentDialog extends PopupWindow {
 
     @CallSuper
     public void onCreate() {
+    }
+
+    @Override
+    public void dismiss() {
+        super.dismiss();
+
+        removeWeakLayoutForActivityContentView();
     }
 
     protected int mTargetGravity = Gravity.FILL;
@@ -128,6 +148,55 @@ public abstract class ContentDialog extends PopupWindow {
             mActivity.getWindowManager().updateViewLayout(decorView, attrs);
         } catch (Throwable e) {
             e.printStackTrace();
+        }
+    }
+
+    private void addWeakLayoutForActivityContentView() {
+        if (mActivityContentView != null) {
+            mActivityContentView.addOnLayoutChangeListener(mWeakLayoutChangedListener);
+        }
+    }
+
+    private void removeWeakLayoutForActivityContentView() {
+        if (mActivityContentView != null) {
+            mActivityContentView.removeOnLayoutChangeListener(mWeakLayoutChangedListener);
+        }
+    }
+
+    private View.OnLayoutChangeListener mActivityContentViewLayoutChangedListener = new View.OnLayoutChangeListener() {
+        @Override
+        public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+            if (mActivityContentView != null) {
+                onActivityContentViewLayoutChanged(mActivityContentView);
+            }
+        }
+    };
+
+    private final WeakLayoutChangedListener mWeakLayoutChangedListener = new WeakLayoutChangedListener(mActivityContentViewLayoutChangedListener);
+
+    public void onActivityContentViewLayoutChanged(@NonNull View contentView) {
+        Log.v(CLASS_NAME, "onActivityContentViewLayoutChanged");
+        View decorView = getDecorView();
+        if (decorView == null) {
+            Log.e(CLASS_NAME, "decor view not found");
+            return;
+        }
+
+        decorView.setPadding(contentView.getPaddingLeft(), contentView.getPaddingTop(), contentView.getPaddingRight(), contentView.getPaddingBottom());
+    }
+
+    public static class WeakLayoutChangedListener extends WeakAvailable implements View.OnLayoutChangeListener {
+
+        public WeakLayoutChangedListener(View.OnLayoutChangeListener listener) {
+            super(listener);
+        }
+
+        @Override
+        public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+            View.OnLayoutChangeListener listener = (View.OnLayoutChangeListener) getObject();
+            if (listener != null) {
+                listener.onLayoutChange(v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom);
+            }
         }
     }
 
