900bb312a5526fa20521b288cba8a03125ea0d74
idonans <idonans@126.com>
2017-09-26 16:50:07 +0800

    adjust fresco memory cache size on 5.0+

--- boot/src/main/java/com/okandroid/boot/data/FrescoManager.java
+++ boot/src/main/java/com/okandroid/boot/data/FrescoManager.java
@@ -1,12 +1,18 @@
 package com.okandroid.boot.data;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.Build;
 
 import com.facebook.cache.disk.DiskCacheConfig;
+import com.facebook.common.internal.Supplier;
 import com.facebook.common.logging.FLog;
 import com.facebook.common.logging.FLogDefaultLoggingDelegate;
+import com.facebook.common.util.ByteConstants;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.imagepipeline.backends.okhttp3.OkHttpNetworkFetcher;
+import com.facebook.imagepipeline.cache.MemoryCacheParams;
 import com.facebook.imagepipeline.core.ImagePipelineConfig;
 import com.okandroid.boot.App;
 import com.okandroid.boot.AppContext;
@@ -55,7 +61,7 @@ public class FrescoManager {
             config = Bitmap.Config.RGB_565;
         }
 
-        ImagePipelineConfig imagePipelineConfig = ImagePipelineConfig.newBuilder(AppContext.getContext())
+        ImagePipelineConfig.Builder imagePipelineConfigBuilder = ImagePipelineConfig.newBuilder(AppContext.getContext())
                 .setMainDiskCacheConfig(DiskCacheConfig.newBuilder(AppContext.getContext())
                         .setBaseDirectoryPath(frescoCacheBaseDir)
                         .setBaseDirectoryName("fresco_main_disk_" + ProcessManager.getInstance().getProcessTag())
@@ -66,10 +72,42 @@ public class FrescoManager {
                         .build())
                 .setNetworkFetcher(new OkHttpNetworkFetcher(OkHttpManager.getInstance().getOkHttpClient()))
                 .setDownsampleEnabled(true)
-                .setBitmapsConfig(config)
-                .build();
+                .setBitmapsConfig(config);
 
-        Fresco.initialize(AppContext.getContext(), imagePipelineConfig);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            imagePipelineConfigBuilder.setBitmapMemoryCacheParamsSupplier(new OKAndroidBitmapMemoryCacheParamsSupplier());
+        }
+
+        Fresco.initialize(AppContext.getContext(), imagePipelineConfigBuilder.build());
+    }
+
+    private static class OKAndroidBitmapMemoryCacheParamsSupplier implements Supplier<MemoryCacheParams> {
+        private static final int MAX_CACHE_ENTRIES = 256;
+        private static final int MAX_EVICTION_QUEUE_SIZE = 1;
+        private static final int MAX_EVICTION_QUEUE_ENTRIES = 1;
+        private static final int MAX_CACHE_ENTRY_SIZE = Integer.MAX_VALUE;
+
+        private final ActivityManager mActivityManager;
+
+        public OKAndroidBitmapMemoryCacheParamsSupplier() {
+            mActivityManager = (ActivityManager) AppContext.getContext().getSystemService(Context.ACTIVITY_SERVICE);
+        }
+
+        @Override
+        public MemoryCacheParams get() {
+            return new MemoryCacheParams(
+                    getMaxCacheSize(),
+                    MAX_CACHE_ENTRIES,
+                    MAX_EVICTION_QUEUE_SIZE,
+                    MAX_EVICTION_QUEUE_ENTRIES,
+                    MAX_CACHE_ENTRY_SIZE);
+        }
+
+        private int getMaxCacheSize() {
+            final int maxMemory =
+                    Math.min(mActivityManager.getMemoryClass() * ByteConstants.MB, Integer.MAX_VALUE);
+            return maxMemory / 6;
+        }
     }
 
 }
