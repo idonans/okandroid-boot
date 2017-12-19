d3644b538e5d4ca5f1d401fe09d2b457f3fdc14f
idonans <idonans@126.com>
2017-12-19 17:06:18 +0800

    fix FileUtil#getFilenameFromUrl, FileUtil#getFileExtensionFromUrl

--- boot/src/main/java/com/okandroid/boot/util/FileUtil.java
+++ boot/src/main/java/com/okandroid/boot/util/FileUtil.java
@@ -14,7 +14,6 @@ import com.okandroid.boot.lang.BootFileProvider;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 /**
  * 文件操作相关辅助类
@@ -233,29 +232,19 @@ public class FileUtil {
      */
     @CheckResult
     public static String getFileExtensionFromUrl(String url) {
-        if (!TextUtils.isEmpty(url)) {
-            int fragment = url.lastIndexOf('#');
-            if (fragment > 0) {
-                url = url.substring(0, fragment);
-            }
-
-            int query = url.lastIndexOf('?');
-            if (query > 0) {
-                url = url.substring(0, query);
-            }
-
-            int filenamePos = url.lastIndexOf('/');
-            String filename =
-                    0 <= filenamePos ? url.substring(filenamePos + 1) : url;
-
-            // if the filename contains special characters, we don't
-            // consider it valid for our matching purposes:
-            if (!filename.isEmpty() &&
-                    Pattern.matches("[a-zA-Z_0-9\\.\\-\\(\\)\\%]+", filename)) {
-                int dotPos = filename.lastIndexOf('.');
-                if (0 <= dotPos) {
-                    return filename.substring(dotPos + 1);
+        String filename = getFilenameFromUrl(url);
+        if (!TextUtils.isEmpty(filename)) {
+            for (; ; ) {
+                if (filename.endsWith(".")) {
+                    filename = filename.substring(0, filename.length());
+                    continue;
                 }
+                break;
+            }
+
+            int dotPos = filename.lastIndexOf('.');
+            if (0 <= dotPos) {
+                return filename.substring(dotPos + 1);
             }
         }
 
@@ -267,18 +256,40 @@ public class FileUtil {
      */
     @CheckResult
     public static String getFilenameFromUrl(String url) {
+        if (url != null) {
+            url = url.trim();
+        }
         if (!TextUtils.isEmpty(url)) {
-            int fragment = url.lastIndexOf('#');
+            if (url.startsWith("#")) {
+                return null;
+            }
+
+            if (url.startsWith("?")) {
+                return null;
+            }
+
+            int fragment = url.indexOf('#');
             if (fragment > 0) {
                 url = url.substring(0, fragment);
             }
 
-            int query = url.lastIndexOf('?');
+            int query = url.indexOf('?');
             if (query > 0) {
                 url = url.substring(0, query);
             }
 
-            int filenamePos = url.lastIndexOf('/');
+            for (; ; ) {
+                if (url.endsWith("/")
+                        || url.endsWith("\\")) {
+                    url = url.substring(0, url.length());
+                    continue;
+                }
+                break;
+            }
+            int filenamePos0 = url.lastIndexOf('/');
+            int filenamePos1 = url.lastIndexOf('\\');
+
+            int filenamePos = Math.max(filenamePos0, filenamePos1);
             String filename =
                     0 <= filenamePos ? url.substring(filenamePos + 1) : url;
             return filename;
